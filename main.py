a3122230eaaa805c581feb1d9a1638c501fd9ea5
sohonr69 <sohonbantawarai@gmail.com>
2022-08-01 01:03:54 +0545

    Day progress added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from calendar import monthcalendar, week
 from datetime import datetime
 from datetime import date
 import calendar
+from time import time
 
 month = date.today().day
 monthDay = calendar.monthrange(datetime.now().year, datetime.now().month)[1]
@@ -13,10 +14,15 @@ weekPercentage = weekDay/7*100
 monthNow = datetime.today().month
 yearPercentage = int(monthNow)/12*100
 
+timeNow = datetime.now().strftime('%I')
+dayPercentage = int(timeNow)/12*100
+
 print("------------------------------------------------")
 print("Year Progress : ", round(yearPercentage),"%")
 
 print("Month Progress : ", round(monthPercentage),"%")
 
 print("Week Progress : ", round(weekPercentage),"%")
+
+print("Day Progress : ", round(dayPercentage),"%")
 print("------------------------------------------------")
